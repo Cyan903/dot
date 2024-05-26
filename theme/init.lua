99859ed3873d6a4410975a1b1bd681ae86258759
Cyan903 <cyanp0515@gmail.com>
2024-05-26 03:17:21 -0400

    feat: Dracula theme

--- theme/init.lua
+++ theme/init.lua
@@ -1,7 +1,5 @@
--- Theme handling library
 local beautiful = require("beautiful")
--- Standard awesome library
 local gears = require("gears")
 
 -- Themes define colors, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_dir("config") .. "theme/dracula/theme.lua")
