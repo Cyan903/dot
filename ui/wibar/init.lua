0edef96e5cd80681e9f5b74de8b178ea68d32cec
Cyan903 <cyanp0515@gmail.com>
2024-06-16 21:28:43 -0400

    feat: Power menu

--- ui/wibar/init.lua
+++ ui/wibar/init.lua
@@ -9,6 +9,7 @@ local ping = require("module.ping.main")
 local volume = require("module.volume.main")
 local packages = require("module.packages.main")
 local time = require("module.time.main")
+local system = require("module.system.main")
 
 return function(s)
     s.mywibox = awful.wibar({
@@ -24,6 +25,7 @@ return function(s)
 
             {
                 layout = wibox.layout.fixed.horizontal,
+
                 module.layoutbox(s),
                 module.taglist(s)
             },
@@ -32,14 +34,15 @@ return function(s)
 
             {
                 layout = wibox.layout.fixed.horizontal,
+
                 wibox.container.margin(taskbar, 4, 4),
                 wibox.container.margin(notifications, 4, 4),
                 wibox.container.margin(ping, 4, 4),
                 wibox.container.margin(volume, 4, 4),
                 wibox.container.margin(packages, 4, 4),
-                -- awful.widget.keyboardlayout(),
                 wibox.container.margin(wibox.widget.textbox("<span weight='bold'> - </span>"), 2, 4),
-                wibox.container.margin(time, 4, 4)
+                wibox.container.margin(time, 4, 4),
+                wibox.container.margin(system, 2, 2)
             }
         },
 
