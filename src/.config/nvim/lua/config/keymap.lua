72ae024b14cc33309bd85a1b0086719ac59f6da6
Cyan903 <cyanp0515@gmail.com>
2025-01-10 21:44:07 -0500

    feat: Quickfix binds

--- src/.config/nvim/lua/config/keymap.lua
+++ src/.config/nvim/lua/config/keymap.lua
@@ -2,7 +2,32 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>qd", vim.diagnostic.setloclist, { desc = "[Q]uickfix [D]iagnostic list" })
+
+-- Quickfix keymaps
+vim.keymap.set("n", "<leader>qq", function()
+    local qf_exists = false
+
+    for _, win in pairs(vim.fn.getwininfo()) do
+        if win["quickfix"] == 1 then
+            qf_exists = true
+        end
+    end
+
+    if qf_exists == true then
+        vim.cmd("cclose")
+        return
+    end
+
+    if not vim.tbl_isempty(vim.fn.getqflist()) then
+        vim.cmd("copen")
+    end
+end, { desc = "[Q]uickfix toggle" })
+
+vim.cmd([[
+    nnoremap <M-j> <cmd>cnext<cr>
+    nnoremap <M-k> <cmd>cprev<cr>
+]])
 
 -- Delete word with CTRL + Backspace
 vim.keymap.set("i", "<C-backspace>", "<C-w>")
@@ -34,20 +59,3 @@ vim.keymap.set("n", "<M-,>", "<c-w>5<")
 vim.keymap.set("n", "<M-.>", "<c-w>5>")
 vim.keymap.set("n", "<M-]>", "<C-W>+")
 vim.keymap.set("n", "<M-[>", "<C-W>-")
-
--- Move lines with ALT j/k
-vim.keymap.set("n", "<M-j>", function()
-    if vim.opt.diff:get() then
-        vim.cmd([[normal! ]c]])
-    else
-        vim.cmd([[m .+1<CR>==]])
-    end
-end)
-
-vim.keymap.set("n", "<M-k>", function()
-    if vim.opt.diff:get() then
-        vim.cmd([[normal! [c]])
-    else
-        vim.cmd([[m .-2<CR>==]])
-    end
-end)
