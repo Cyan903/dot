10565c04169b32ddd45ece621b32a7b1bbb9ffb7
Cyan903 <cyanp0515@gmail.com>
2025-10-11 16:53:39 -0400

    rework: Remove scripts-wayland

--- eggs/nvim/lua/addons/plugins/gremlins.lua
+++ eggs/nvim/lua/addons/plugins/gremlins.lua
@@ -7,10 +7,10 @@ return {
     config = function(_, opts)
         local gremlins = require("gremlins")
 
-        ---Open popup.
+        -- Open popup
         vim.keymap.set("n", "<leader>gs", gremlins.select, { desc = "Gremlins select." })
 
-        ---Create quickfix list.
+        -- Create quickfix list
         vim.keymap.set("n", "<leader>gc", gremlins.qflist, { desc = "Gremlins quickfix." })
 
         gremlins.setup(opts)
